849dca5ef5d3846f95df409757cded39f59d18d5
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2025-01-27 16:29:18 +0300

    feat: Query client SSR setup

--- app/src/providers/query-provider.tsx
+++ app/src/providers/query-provider.tsx
@@ -8,9 +8,20 @@ type Props = {
   children: React.ReactNode;
 };
 
-const queryClient = new QueryClient();
-
 export default function QueryProvider({ children }: Props) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}

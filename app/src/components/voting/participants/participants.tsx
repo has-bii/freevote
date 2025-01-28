7c55c2e43cb6b20bdeb515b7c170e4ccba728001
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2025-01-28 17:44:10 +0300

    feat: participants table

--- app/src/components/voting/participants/participants.tsx
+++ app/src/components/voting/participants/participants.tsx
@@ -5,22 +5,13 @@ import {
   UseGetParticipantsParams,
 } from "@/hooks/participants/use-get-participants";
 import React from "react";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { format } from "date-fns";
-import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
 import RemoveParticipant, {
   TParticipantRemove,
 } from "./remove/remove-participant";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { RefreshCw, Trash2 } from "lucide-react";
+import { RefreshCw } from "lucide-react";
+import { DataTable } from "./table/data-table";
+import { columns } from "./table/columns";
 
 interface Props extends UseGetParticipantsParams {
   id: string;
@@ -45,74 +36,7 @@ export default function Participants({ id, initialData }: Props) {
         </Button>
       </div>
 
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>#</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead className="text-right">Joined At</TableHead>
-            <TableHead className="sr-only">Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {participants === undefined ? (
-            Array.from({ length: 10 }).map((_, i) => (
-              <TableRow key={i}>
-                <TableCell colSpan={4}>
-                  <Skeleton className="h-8 w-full" />
-                </TableCell>
-              </TableRow>
-            ))
-          ) : participants.length === 0 ? (
-            <TableRow>
-              <TableCell colSpan={3}>
-                <div className="flex h-28 w-full items-center justify-center">
-                  <p className="text-muted-foreground">No participant exists</p>
-                </div>
-              </TableCell>
-            </TableRow>
-          ) : (
-            participants.map((participant, i) => {
-              const { id, profiles, created_at } = participant;
-
-              return (
-                <TableRow key={id}>
-                  <TableCell className="font-medium">{i + 1}</TableCell>
-                  <TableCell>
-                    <div className="inline-flex items-center gap-2">
-                      <Avatar>
-                        <AvatarImage src={profiles.avatar ?? undefined} />
-                        <AvatarFallback>
-                          {profiles.full_name
-                            .split(" ")
-                            .map((c) => c[0].toUpperCase())
-                            .join("")}
-                        </AvatarFallback>
-                      </Avatar>
-                      <p>{profiles.full_name}</p>
-                    </div>
-                  </TableCell>
-                  <TableCell className="text-right">
-                    {format(created_at, "PP p")}
-                  </TableCell>
-                  <TableCell>
-                    <div className="flex items-center justify-end gap-2">
-                      <Button
-                        size="sm"
-                        variant="destructive"
-                        onClick={() => setRemoveData(participant)}
-                      >
-                        <Trash2 />
-                        Remove
-                      </Button>
-                    </div>
-                  </TableCell>
-                </TableRow>
-              );
-            })
-          )}
-        </TableBody>
-      </Table>
+      <DataTable columns={columns} data={participants} />
 
       <RemoveParticipant
         open={removeData !== undefined}
---
+++ app/src/components/voting/participants/table/columns.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { TParticipant, TProfile } from "@/types/model";
+import { ColumnDef } from "@tanstack/react-table";
+import { compareAsc, format } from "date-fns";
+import { ArrowUpDown, Trash2 } from "lucide-react";
+
+type TData = TParticipant & {
+  profiles: Pick<TProfile, "full_name" | "avatar">;
+};
+
+export const columns: ColumnDef<TData>[] = [
+  {
+    accessorKey: "profiles",
+    sortingFn: (
+      { original: { profiles: profiles1 } },
+      { original: { profiles: profiles2 } },
+    ) => profiles1.full_name.localeCompare(profiles2.full_name),
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Full Name
+          <ArrowUpDown className="h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({
+      row: {
+        original: { profiles },
+      },
+    }) => (
+      <div className="ml-4 inline-flex items-center gap-2">
+        <Avatar>
+          <AvatarImage src={profiles.avatar ?? undefined} />
+          <AvatarFallback>
+            {profiles.full_name
+              .split(" ")
+              .map((c) => c[0].toUpperCase())
+              .join("")}
+          </AvatarFallback>
+        </Avatar>
+        <p>{profiles.full_name}</p>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "created_at",
+    sortingFn: (
+      { original: { created_at } },
+      { original: { created_at: created_at2 } },
+    ) => compareAsc(created_at, created_at2),
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Joined at
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({
+      row: {
+        original: { created_at },
+      },
+    }) => (
+      <span className="text-sm text-muted-foreground">
+        {format(created_at, "PP p")}
+      </span>
+    ),
+  },
+  {
+    id: "actions",
+    cell: ({}) => (
+      <div className="flex justify-end pr-2">
+        <Button size="sm" variant="destructive">
+          <Trash2 />
+          Remove
+        </Button>
+      </div>
+    ),
+  },
+];

c031d6d0ff5763ab85f80fd82820cf7f093344f6
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2025-01-28 05:31:37 +0300

    feat: remote image config

--- app/next.config.ts
+++ app/next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "54321",
+        protocol: process.env.REMOTE_IMG_PROTOCOL as "https" | "http",
+        hostname: process.env.REMOTE_IMG_HOSTNAME as string,
+        port: process.env.REMOTE_IMG_PORT,
         pathname: "/storage/v1/object/public/**",
       },
     ],

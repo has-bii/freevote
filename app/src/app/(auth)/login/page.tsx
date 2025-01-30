eee39cc39d637f0ccf9d00e9f1738cd00781fb3f
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2025-01-30 18:47:20 +0300

    style: remove google

--- app/src/app/(auth)/login/page.tsx
+++ app/src/app/(auth)/login/page.tsx
@@ -51,9 +51,9 @@ export default function LoginPage() {
           <Input id="password" name="password" type="password" required />
         </div>
         <Button type="submit" className="w-full" disabled={isPending}>
-          Login {isPending && <Loader className="animate-spin" />}
+          {isPending && <Loader className="animate-spin" />}Login
         </Button>
-        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+        {/* <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
           <span className="relative z-10 bg-background px-2 text-muted-foreground">
             Or continue with
           </span>
@@ -66,7 +66,7 @@ export default function LoginPage() {
             />
           </svg>
           <span>Login with Google</span>
-        </Button>
+        </Button> */}
         <div className="text-center text-sm">
           Don&apos;t have an account?&nbsp;
           <Link href="/register" className="underline underline-offset-4">

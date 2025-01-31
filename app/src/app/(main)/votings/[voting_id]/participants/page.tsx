28479d0a40ae45231048574dd067a059a98e7b23
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2025-01-31 07:43:23 +0300

    fix: caching

--- app/src/app/(main)/votings/[voting_id]/participants/page.tsx
+++ app/src/app/(main)/votings/[voting_id]/participants/page.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Participants from "@/components/voting/participants/participants";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
-import { getVotingByIdCached } from "@/app/(api)/api/voting/[voting_id]/get-voting-by-id-cached";
 import { getParticipantCached } from "@/app/(api)/api/participant/[voting_id]/get-participant-cached";
+import { getVotingByIdCached } from "@/app/(api)/api/voting/[voting_id]/get-voting-by-id-cached";
 
 type Props = {
   params: Promise<{ voting_id: string }>;
@@ -11,21 +9,15 @@ type Props = {
 
 export default async function VotingParticipantsPage({ params }: Props) {
   const { voting_id } = await params;
-  const supabase = await createClient();
-
-  const { data } = await getVotingByIdCached(voting_id);
-
-  if (!data) redirect("/votings");
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user) redirect("/login");
-
-  if (user.id !== data.user_id) redirect(`/votings/${voting_id}/vote`);
 
   const { data: participantsData } = await getParticipantCached(voting_id);
+  const { data: votingData } = await getVotingByIdCached(voting_id);
 
-  return <Participants id={voting_id} initialData={participantsData ?? []} />;
+  return (
+    <Participants
+      id={voting_id}
+      initialData={participantsData ?? []}
+      votingData={votingData}
+    />
+  );
 }
--- app/src/components/voting/participants/participants.tsx
+++ app/src/components/voting/participants/participants.tsx
@@ -9,17 +9,31 @@ import { Button } from "@/components/ui/button";
 import { RefreshCw } from "lucide-react";
 import { DataTable } from "./table/data-table";
 import { columns } from "./table/columns";
+import { useSupabase } from "@/utils/supabase/client";
+import { useGetAuth } from "@/hooks/auth/use-auth";
+import { TVoting } from "@/types/model";
+import { useRouter } from "next/navigation";
 
 interface Props extends UseGetParticipantsParams {
   id: string;
+  votingData: TVoting | null;
 }
 
-export default function Participants({ id, initialData }: Props) {
+export default function Participants({ id, initialData, votingData }: Props) {
+  const supabase = useSupabase();
+  const { data: user } = useGetAuth(supabase);
+  const router = useRouter();
   const {
     data: participants,
     refetch,
     isRefetching,
   } = useGetParticipants({ id, initialData });
+
+  React.useEffect(() => {
+    if (!user || !votingData) return;
+
+    if (user.id !== votingData.user_id) router.push(`/votings/${id}/vote`);
+  }, [id, router, user, votingData]);
 
   return (
     <div className="space-y-2 p-4 pt-0">

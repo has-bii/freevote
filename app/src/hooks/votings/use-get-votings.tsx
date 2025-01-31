454b73bb412e279c7717ccc0fb844ca2f7611d40
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2025-01-31 06:43:33 +0300

    feat: add order & remove fetching user

--- app/src/hooks/votings/use-get-votings.tsx
+++ app/src/hooks/votings/use-get-votings.tsx
@@ -69,19 +69,10 @@ export const useGetJoinedVotings = (supabase: TSupabaseClient) =>
   useQuery({
     queryKey: ["joined votings"],
     queryFn: async () => {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-
-      if (!user) {
-        toast.error("Log in first");
-        throw new Error("User is not authenticated");
-      }
-
       const { data, error } = await supabase
         .from("voters")
         .select("*,votings(*)")
-        .eq("user_id", user.id);
+        .order("created_at", { ascending: false });
 
       if (error) throw new Error(error.message);
 

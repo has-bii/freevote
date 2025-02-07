e24f63157df5522b14b4144955b0368813ed444e
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2025-02-07 11:20:31 +0300

    fix: type

--- app/src/components/voting/result/select-sessions.tsx
+++ app/src/components/voting/result/select-sessions.tsx
@@ -1,36 +1,37 @@
-"use client";
+"use client"
 
-import { TSession } from "@/types/model";
+import { TSession } from "@/types/model"
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
-import React from "react";
-import { useRouter } from "next/navigation";
-import { isPast } from "date-fns";
+} from "@/components/ui/select"
+import React from "react"
+import { useRouter } from "next/navigation"
+import { isPast } from "date-fns"
 
 type Props = {
-  voting_id: string;
-  data: TSession[];
-  current: string;
-};
+  voting_id: string
+  data: TSession[]
+  current: string
+}
 
 export default function SelectSessions({ data, current, voting_id }: Props) {
-  const router = useRouter();
+  const router = useRouter()
 
   const filtered = React.useMemo(
-    () => data.filter((d) => isPast(d.session_end_at)),
+    () =>
+      data.filter((d) => (d.session_end_at ? isPast(d.session_end_at) : false)),
     [data],
-  );
+  )
 
   return (
     <Select
       defaultValue={current}
       onValueChange={(v) => {
-        router.push(`/votings/${voting_id}/result/${v}`);
+        router.push(`/votings/${voting_id}/result/${v}`)
       }}
     >
       <SelectTrigger className="w-[180px]">
@@ -44,5 +45,5 @@ export default function SelectSessions({ data, current, voting_id }: Props) {
         ))}
       </SelectContent>
     </Select>
-  );
+  )
 }
